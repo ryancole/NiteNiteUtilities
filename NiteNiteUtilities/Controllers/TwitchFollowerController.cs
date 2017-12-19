b852141c6a3e1b5ff62d9b1bf09ea8719afed4a9
Ryan Cole <ryan@rycole.com>
2017-12-18 22:17:39 -0600

    include build in repo

--- NiteNiteUtilities/Controllers/TwitchFollowerController.cs
+++ NiteNiteUtilities/Controllers/TwitchFollowerController.cs
@@ -13,24 +13,17 @@ namespace NiteNiteUtilities.Controllers
 
         async public Task<TwitchUser> Get()
         {
-            try
-            {
-                // all we want to do is pop the next follower off the queue so that
-                // the browser source can be unloaded and we won't lose track of
-                // any followers in the process
-                var follower = PersistantRuntimeData.Followers.Dequeue();
+            // all we want to do is pop the next follower off the queue so that
+            // the browser source can be unloaded and we won't lose track of
+            // any followers in the process
+            var follower = PersistantRuntimeData.Followers.Dequeue();
 
-                // now we need to out to twitch again and fetch the display name of
-                // the person who followed
-                var details = await TwitchUserRepository.GetById(follower.Data.From);
+            // now we need to out to twitch again and fetch the display name of
+            // the person who followed
+            var details = await TwitchUserRepository.GetById(follower.Data.From);
 
-                // we only want to respond with the user details
-                return details.Users.Single();
-            }
-            catch
-            {
-                return null;
-            }
+            // we only want to respond with the user details
+            return details.Users.Single();
         }
 
         #endregion
---
+++ NiteNiteUtilities/Public/script/client.js
@@ -0,0 +1,45 @@
+
+// get the dom element that contains the follower name
+const followerElement = document.getElementById("follower");
+
+async function fetchNextFollower() {
+	// we need to fetch the details of a most recent follower, if any
+	const response = await fetch("http://localhost:8080/api/twitchfollower");
+
+	// there may not be a recent follower to show
+	if (response.status != 200) {
+		return;
+	}
+
+	// now that we got a response, we need to convert it to json
+	const json = await response.json();
+
+	return json;
+}
+
+async function displayNextFollower() {
+	try {
+		// first we gotta fetch a follower
+		const follower = await fetchNextFollower();
+
+		// set the element text to the follower name
+		followerElement.textContent = follower.display_name;
+
+		// add the animation class
+		followerElement.classList.add("slideLeft");
+	} catch (ex) {}
+
+	// in ten seconds we want to hide the follower
+	setTimeout(hideCurrentFollower, 10000);
+}
+
+function hideCurrentFollower() {
+	// remove the animation class, which also hides the element
+	followerElement.classList.remove("slideLeft");
+
+	// in two seconds we want to show the next follower
+	setTimeout(displayNextFollower, 2000);
+}
+
+// in five seconds we want to start showing followers
+setTimeout(displayNextFollower, 5000);
