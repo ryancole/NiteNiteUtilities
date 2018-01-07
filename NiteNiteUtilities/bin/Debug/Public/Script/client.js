e5fce42ec8acd5b75dae0873ff584fde601f2ac6
Ryan Cole <ryan@rycole.com>
2018-01-06 20:24:47 -0600

    try some animations

--- NiteNiteUtilities/bin/Debug/Public/Script/client.js
+++ NiteNiteUtilities/bin/Debug/Public/Script/client.js
@@ -2,7 +2,10 @@
 const toastyElement = document.getElementById("toasty");
 
 const followerElement = document.getElementById("follower");
-const followerContainerElement = document.getElementById("followerContainer");
+
+const nameSlideDownElement = document.getElementById("nameSlideDown");
+const nameSlideDownContainerElement = document.getElementById("nameSlideDownContainer");
+const nameSlideDownParagraphElements = document.querySelectorAll("#nameSlideDownContainer p");
 
 async function fetchNextFollower() {
 	// we need to fetch the details of a most recent follower, if any
@@ -20,23 +23,31 @@ async function fetchNextFollower() {
 }
 
 async function displayNextFollower() {
-	// first we gotta fetch a follower
-	const follower = await fetchNextFollower();
-
-	if (follower) {
-		// set the element text to the follower name
-		followerElement.textContent = follower.display_name;
-
-		// add the animation class
-		followerContainerElement.classList.remove("invisible");
-
-		// play the toasty sound
-		toastyElement.play();
-	}
-
-	// in ten seconds we want to show the next follower
-	setTimeout(displayNextFollower, 10000);
+  const follower = await fetchNextFollower();
+  if (follower) {
+    beginAnimation(follower);
+    toastyElement.play();
+  }
 }
 
-// in five seconds we want to start showing followers
-setTimeout(displayNextFollower, 5000);
+function beginAnimation(follower) {
+  nameSlideDownElement.textContent = follower.display_name;
+  nameSlideDownContainerElement.classList.remove("invisible");
+  nameSlideDownParagraphElements.forEach(e => e.classList.add("animated"));
+}
+
+function handleAnimationEnd(event) {
+  if (event.animationName == "slideUpRight") {
+    const name = event.target.textContent;
+	followerElement.textContent = name;
+	nameSlideDownContainerElement.classList.add("invisible");
+	nameSlideDownParagraphElements.forEach(e => e.classList.remove("animated"));
+	setTimeout(displayNextFollower, 1000);
+  }
+}
+
+// register our event listeners
+nameSlideDownElement.addEventListener("animationend", handleAnimationEnd, false);
+
+// in seconds we want to start showing followers
+setTimeout(displayNextFollower, 2000);
